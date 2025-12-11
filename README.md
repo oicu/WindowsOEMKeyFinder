4c4f43e159871690520ee0394704dca57ba9fa02
oicu <oicu.cc@163.com>
2025-12-11 15:12:35 +0800

    增加 PowerShell 脚本方式查询序列号

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
-使用 vbs 脚本查看并备份 Win7/Win8/Win10 当前系统使用的序列号、主板里内置的 OEM 序列号及描述。
+使用 vbs、PowerShell 脚本查看并备份 Win7/Win8/Win10 当前系统使用的序列号、主板里内置的 OEM 序列号及描述。
 
 解决预装 OEM 系统的电脑，在重装系统后找不到原来序列号的问题。
 
-![example](https://raw.githubusercontent.com/oicu/WindowsOEMKeyFinder/main/WindowsOEMKeyFinder-example.png)
+![example](https://raw.githubusercontent.com/oicu/WindowsOEMKeyFinder/main/WindowsOEMKeyFinder-vbs.png)
+
+![example](https://raw.githubusercontent.com/oicu/WindowsOEMKeyFinder/main/WindowsOEMKeyFinder-ps.png)
 
 Description 的含义：
  - [4.0] CoreCountrySpecific OEM:DM 表示这个序列号可激活家庭中文版。
  - [4.0] Professional OEM:DM 表示这个序列号可激活专业版。
  - 如果有 OEM 序列号但描述为空，则这个序列号和当前系统不匹配，比如当前系统是 Win10，但 OEM 序列号是用于 Win8 的。
 
-该脚本经过上万台电脑测试，如果安装密钥 Installed Key 是 `BBBBB-BBBBB-BBBBB-BBBBB-BBBBB`，先用`slmgr /ipk`导入其他密钥，再导入原来的密钥！直接导入原来密钥不会触发更改。
+vbs 脚本经过上万台电脑测试，如果安装密钥 Installed Key 是 `BBBBB-BBBBB-BBBBB-BBBBB-BBBBB`，先用`slmgr /ipk`导入其他密钥，再导入原来的密钥！直接导入原来密钥不会触发更改。
 
 vbs 脚本部分功能参考：
 
 https://winaero.com/how-to-view-your-product-key-in-windows-10-windows-8-and-windows-7/
 https://gist.github.com/craigtp/dda7d0fce891a087a962d29be960f1da
+
+PowerShell 脚本未经过大量测试，可靠性未知。
 
 ---
 
---
+++ WindowsOEMKeyFinder.ps1
@@ -0,0 +1,76 @@
+﻿<#
+https://github.com/oicu
+2025/12/10
+Backup Windows Key, Windows Product Key Finder, Windows OEM Find Finder
+
+修改执行策略以允许脚本运行:
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+#>
+
+function ConvertToKey {
+    param(
+        [byte[]]$Key
+    )
+    $Maps = "BCDFGHJKMPQRTVWXY2346789"
+
+    # Check if OS is Windows 8
+    $isWin8 = ([math]::Truncate($Key[14] / 6)) -band 1
+    $Key[14] = ($Key[14] -band 0xF7) -bor (($isWin8 -band 2) * 4)
+
+    $KeyOutput = ""
+    $Last = 0
+
+    for ($i = 24; $i -ge 0; $i--) {
+        $Current = 0
+        for ($j = 14; $j -ge 0; $j--) {
+            $Current = $Current * 256
+            $Current = $Key[$j] + $Current
+            $Key[$j] = [math]::Truncate($Current / 24)
+            $Current = $Current % 24
+        }
+        $KeyOutput = $Maps[$Current] + $KeyOutput
+        $Last = $Current
+    }
+
+    if ($isWin8 -eq 1) {
+        if ($Last -gt 0) {
+            $keypart1 = $KeyOutput.Substring(1, $Last)
+            $KeyOutput = $KeyOutput.Substring(0, 1) + $keypart1 + "N" + $KeyOutput.Substring(1 + $Last)
+            $KeyOutput = $KeyOutput.Substring(1)
+        } else {
+            $KeyOutput = "N" + $KeyOutput.Substring(1)
+        }
+    }
+
+    $formattedKey = $KeyOutput.Substring(0, 5) + "-" +
+                    $KeyOutput.Substring(5, 5) + "-" +
+                    $KeyOutput.Substring(10, 5) + "-" +
+                    $KeyOutput.Substring(15, 5) + "-" +
+                    $KeyOutput.Substring(20, 5)
+
+    return $formattedKey
+}
+
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+$dp  = Get-ItemPropertyValue -Path $regPath -Name "DigitalProductId"
+#$dp4 = Get-ItemPropertyValue -Path $regPath -Name "DigitalProductId4"
+$ProductName = Get-ItemPropertyValue -Path $regPath -Name "ProductName"
+$ProductID = Get-ItemPropertyValue -Path $regPath -Name "ProductID"
+$sls = Get-CimInstance -query 'select * from SoftwareLicensingService'
+
+$ProductKey = ConvertToKey -Key $dp[52..67]
+#$ProductKey = ConvertToKey -Key $dp4[808..823]
+$OEMKey = $sls.OA3xOriginalProductKey
+$OEMKeyDesc = $sls.OA3xOriginalProductKeyDescription
+Write-Host ("=" * 78)
+Write-Host "查看 Win7、Win8、Win10 使用的序列号、主板 OEM 序列号。`n"
+Write-Host "Product Name: $ProductName"
+Write-Host "Product ID: $ProductID"
+Write-Host "Installed Key: $ProductKey`n"
+Write-Host "OEM Key: $OEMKey"
+Write-Host "Description: $OEMKeyDesc"
+Write-Host ("=" * 78)
+#Read-Host -Prompt "按下回车键以退出..."
+Write-Host "按任意键继续..."
+[System.Console]::ReadKey($true) | Out-Null
